c97ba29b8153f488a22549de03089873a9a5bba5
reedorpenwork <reed.orpen@dfo-mpo.gc.ca>
2026-02-24 09:54:36 -0800

    Added description and oxygen

--- R/cpue.R
+++ R/cpue.R
@@ -1,3 +1,15 @@
+#' Calculate Catch Per unit effort
+#'
+#' @param catch numeric vector of catch
+#' @param effort numeric vector of effort
+#' @param gear_factor numeric adjustment for gear standard
+#'
+#' @returns numeric vector of CPUE values
+#' @export
+#'
+#' @examples
+#' cpue(100,10)
+#' cpue(100, 10, gear_factor = .5)
 cpue <- function(catch, effort, gear_factor = 1) {
   raw_cpue <- catch / effort
 
